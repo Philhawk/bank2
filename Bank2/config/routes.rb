ac329c679920df056e5e71dbc31ee1780117f8a4
Philip Jacob <pkjacob@gmail.com>
2014-11-15 09:54:57 +1100

    updating latest

--- Bank2/config/routes.rb
+++ Bank2/config/routes.rb
@@ -22,7 +22,7 @@
 Rails.application.routes.draw do
   resources :transactions
 
-  resources :accounts
+  root :to 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- Bank2/db/seeds.rb
+++ Bank2/db/seeds.rb
@@ -1,4 +1,4 @@
 Account.create({ name: 'Everyday', balance: 1000 })
 Account.create({ name: 'Savings1', balance: 2000 })
 Account.create({ name: 'Savings2', balance: 25000 })
-Account.create({ name: 'BonusSavings', balance: 5000 })
+Account.create({ name: 'BonusSavings', balance: 5000 }) 	
